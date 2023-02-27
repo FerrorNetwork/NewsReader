f3929575b4280e913ee58759f71e3969dc529a46
FerrorNetwork <40063558+FerrorNetwork@users.noreply.github.com>
2023-02-27 14:00:11 +0400

    Initial Commit

---
+++ NewsReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by Данил on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
